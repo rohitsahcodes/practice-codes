abc68f1b5cca3a300cb25e7ddc3501acab134508
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-07 20:20:25 +0545

    function basic practice committed
    from javascript.info/function-basics function exercise and odin project exercise
    completed

--- script.js
+++ script.js
@@ -1,59 +1,94 @@
 
 
-// javascript.info/switch
+// //javascript
 
-// //change the switch to if else
-// switch (browser) {
-//   case 'Edge':
-//     alert( "You've got the Edge!" );
-//     break;
-
-//   case 'Chrome':
-//   case 'Firefox':
-//   case 'Safari':
-//   case 'Opera':
-//     alert( 'Okay we support these browsers too' );
-//     break;
-
-//   default:
-//     alert( 'We hope that this page looks ok!' );
+// function favoriteAnimal(animal) {
+//   return animal + " is my favorite animal!";
 // }
-// //Answer
-let browser = prompt('Baby! which browse you use?', '')
-if (browser === 'Edge') alert("You've got the Edge!");
-else if (browser === 'Chrome'
-  || browser === 'Firefox'
-  || browser === 'Safari'
-  || browser === 'Opera') alert('Okay we support these browsers too');
-else if (browser === '') alert('Idiot! type carefully');
-else if (browser === null) alert('Cancelled');
-else alert('We hope that this page looks ok!');
+// const message = favoriteAnimal('Goatjhjss');
+// console.log(message);
 
-// //change if else to switch
-// let a = +prompt('a?', '');
 
-// if (a == 0) {
-//   alert( 0 );
+// //min value finder function
+// function min(a, b) {
+//   return (a > b) ? b : a;
 // }
-// if (a == 1) {
-//   alert( 1 );
+// console.log(min(39, -46));
+
+
+// //Power function
+// function pow(x, y) {
+//   let z = x;
+//   for (let i = 1; i < y; i++) {
+//     z *= x;
+//   }
+//   return z;
+// }
+// console.log(pow(2, 4));
+
+
+// //function value change inside and outside... when called it now assigh new value
+
+// let userName = 'Rohit';
+// function showMessage(){
+//   userName = 'Nitesh';
+//   let print = "Hi, " +userName;
+//   console.log(print);
+// }
+// console.log(userName); //Rohit because function is not called till now
+
+// showMessage();
+
+// console.log(userName);  //Nitesh because function is called and new value assigned
+
+
+// //now we declare userName 2 times.. so inside function is different thant outside function
+// let userName = 'Rohit';
+// function showMessage(){
+//   let userName = 'Nitesh';
+//   let print = "Hi, " +userName;
+//   console.log(print);
+// }
+// console.log(userName);
+
+// showMessage();
+
+// console.log(userName);
+
+
+// // difference of local copy and original/global
+// function showMessage(from, text) {
+
+//   from = '*' + from + '*'; // make "from" look nicer
+
+//   console.log( from + ': ' + text );
 // }
 
-// if (a == 2 || a == 3) {
-//   alert( '2,3' );
+// let from = "Ann";
+
+// showMessage(from, "Hello"); // *Ann*: Hello
+
+// // the value of "from" is the same, the function modified a local copy
+// console.log( from ); // Ann
+
+// //main if else qns
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Did parents allow you?');
+//   }
 // }
 
-// Answer
-let a = +prompt('a?', '')
-switch (a){
-  case 0:
-    alert(0);
-    break;
-  case 1:
-    alert(1);
-    break;
-  case 2:
-  case 3:
-    alert('2,3');
-    break;
-}
+////written in ? : conditional
+// function checkAge(age){
+//   let permission = (age > 18) ? true : comfirm ('Did parents allow you?');
+//   return permission;
+// }
+
+// //written in or conditional
+// function checkAge(age){
+//   let permission = (age > 18) || confirm('Did parents allow you?');
+//   return permission;
+// }
+// console.log(checkAge(47));
