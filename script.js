4e311a6d3a915734252737e284f54cc1c331c452
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-10 22:42:12 +0545

    fizz buzz game

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 
-//write a function called add7 that takes one number and returns thet number + 7;
-function add7 (a){
-  return a + 7;
-}
-console.log(add7(10));
+//fizz buzz game 
+function fizzBuzz(value) {
+  let m3 = value % 3;
+  let m5 = value % 5;
 
-
-//Wrte a function called multiply that takes 2 numbers and returns their product.
-function multiply(a, b){
-  return a * b;
-}
-console.log(multiply(3, 2));
-
-
-//Write a function called capitalized that takes a string and returns that string with only the first letter capitalized. Make sure it can take strings that are lowercase, UPPERCASE or BoTh.
-function capitalize(a){
-  let result;
-  result = a.toLowerCase();
-  result = result.charAt(0).toUpperCase() + result.slice(1);
-  return result;
-}
-console.log(capitalize("helloHOwAreYou"));
-
-
-//Write a function callet lastLetter that takes a string and returns the very last letter of that string;
-
-function lastLetter(a){
-  let result;
-  result = a.slice(-1);
-  return result;
+  if (m3 === 0 && m5 === 0) {
+    console.log("fizzbuzz");
+  }
+  else if (m3 === 0) {
+    console.log("fizz");
+  }
+  else if (m5 === 0) {
+    console.log("buzz")
+  }
+  else {
+    console.log(value);
+  }
 }
 
-console.log(lastLetter("ILoveProgramming"));
+let takenValue = 50;
+
+
+for (let i = 1; i <= takenValue; i++){
+  fizzBuzz(i);
+}
+
