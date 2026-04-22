570235ecab73c330114f14bb0355ec47ee120b45
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-22 20:42:16 +0545

    js.info task comment was my soln

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
 
+// let array = [5, 8, 3, 1];
 
 
+// function filterRangeInPlace(arr, a, b) {
 
-
-//camelizing using "for" loop and "map" method
-
-// //using "for loop"
-// function camelize(str) {
-//   let container = [];
-//   let splitted = str.split("-");
-//   console.log(splitted);  // splitted = ['background', 'color']
-//   for (let i = 0; i < splitted.length; i++) {
-//     console.log(splitted.length, i)
-//     if (i === 0) container.push(splitted[i]);
-//     else container.push(splitted[i].charAt(0).toUpperCase() + splitted[i].slice(1));
-//     console.log("container : ", container);
-//   }
-
-//   let result = container.join('');
-//   return result;
-
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!(arr[i] <= b && arr[i] >= a)) {
+//       arr.splice(i, 1);
+//       i--;
+//     } 
+//   };
 // }
 
+// let filtered = filterRangeInPlace(array, 1, 4);
+// console.log(filtered);
+// console.log(array)
 
-// using map methods
-function camelize(str){
-  let container = [];
-  let splitted = str.split("-");    // splitted = ['background', 'color']
-  let mapp = splitted.map((item, index) => (index === 0) ? item : item.charAt(0).toUpperCase() + item.slice(1));
-  mapp = mapp.join('');
-  console.log(mapp);
+
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    // remove if outside of the interval
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
 }
 
+let arr = [5, 8, 3, 1];
 
-console.log(camelize("background-color-hlo"));
+console.log(filterRangeInPlace(arr, 1, 4)); // removed the numbers except from 1 to 4
 
-
-
-
-
-
-
+console.log( arr ); // [3, 1]
 
 
 
