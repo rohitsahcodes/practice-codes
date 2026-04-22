9bab1bc4cc52bfab473a9324f8ea869bb53184d4
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-22 16:55:56 +0545

    Odin sum of tripled even exercise using reduce method

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
 
-//basic "reduce" method and "for" loop
 
 
-//calculating multiplication of all arr values
+
+// make even number triple and sum them;
+
 let arr = [1, 2, 3, 4, 5, 6];
+function sumOfTripledEvens() {
+  let result =  arr.reduce((acc, curr) => {
+    if (curr % 2 === 0) curr *= 3;
+    acc += curr;
+    return acc;
+  }, 0);
+  console.log(`The sum or vlues of array when even value is triples is : ${result}`);
 
-//using for loop
-let total = 1;
-for (let i = 0; i < arr.length; i++) {
-  total = total * arr[i];
-}
-console.log(total);
-
-
-//using fuction and reduce
-function reducee(acc, curr){
-  return acc *= curr;
 }
 
-let result = arr.reduce(reducee);
-console.log(result);
-
-
-//using arrow function
-let ans = arr.reduce((acc, curr) => {
-  return acc *= curr;
-
-},2);    //2 accumulator value does (2 * ${ans})
-// let ans = arr.reduce((acc, curr) => acc *= curr);
-
-console.log (ans);
-
-
-
-
-
+console.log(arr);
+sumOfTripledEvens();
 
 
 
