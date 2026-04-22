ad15ab6fddecf3658375916d4254c931152cd4e8
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-22 21:47:52 +0545

    Suffle code

--- script.js
+++ script.js
@@ -1,75 +1,42 @@
 
-// let array = [5, 8, 3, 1];
 
 
-// function filterRangeInPlace(arr, a, b) {
-
-//   for (let i = 0; i < arr.length; i++) {
-//     if (!(arr[i] <= b && arr[i] >= a)) {
-//       arr.splice(i, 1);
-//       i--;
-//     } 
-//   };
-// }
-
-// let filtered = filterRangeInPlace(array, 1, 4);
-// console.log(filtered);
-// console.log(array)
 
 
-function filterRangeInPlace(arr, a, b) {
 
-  for (let i = 0; i < arr.length; i++) {
-    let val = arr[i];
 
-    // remove if outside of the interval
-    if (val < a || val > b) {
-      arr.splice(i, 1);
-      i--;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function suffle(arr){
+  for (let i = 0; i < arr.length; i++){
+    let r = Math.floor(Math.random() * arr.length);
+    let temp = arr[i];
+    arr[i] = arr[r];
+    arr[r]= temp;
   }
-
 }
-
-let arr = [5, 8, 3, 1];
-
-console.log(filterRangeInPlace(arr, 1, 4)); // removed the numbers except from 1 to 4
-
-console.log( arr ); // [3, 1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let arr = [1, 2, 3];
+suffle(arr);
+console.log(arr);
