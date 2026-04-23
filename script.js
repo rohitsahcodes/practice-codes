c0697205687263764f565c9cd8a7ab166855d6a3
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-23 12:03:12 +0545

    Seperate number and char form mix array...

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-const str = "Heena";
-function checkRepetedLetter(str) {
-  let temp = "";
-  for (let i = 0; i < str.length; i++) {
-    if(str[i] === temp){
-      console.log(`The first repeted letter is "${str[i]}"`);
-      break;
-    }
-    temp = str[i];
+
+const arr = ['a', 'b', 18, 'd', 100, 10];
+
+function seperateNumStr(arr){
+  let numArr = [];
+  let charArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] === "string") charArr.push(arr[i]);
+    if(typeof arr[i] === "number") numArr.push(arr[i]);
   }
+  console.log(numArr);
+  console.log(charArr);
 }
 
-
-
-
-
-checkRepetedLetter(str);
-
-
+seperateNumStr(arr);
 
 
 
