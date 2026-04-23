6b24ee9b829e8cb7359602dd55da12d2c90a3f8d
rohitsahcodes <rohitsah.codes@gmail.com>
2026-04-23 13:15:30 +0545

    Find duplicate num from given arr

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
-const arr = ['a', 'b', 18, 'd', 100, 10];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 2, 4, 5];
 
-function seperateNumStr(arr){
-  let numArr = [];
-  let charArr = [];
-  for(let i = 0; i < arr.length; i++){
-    if(typeof arr[i] === "string") charArr.push(arr[i]);
-    if(typeof arr[i] === "number") numArr.push(arr[i]);
+//function to create new arr of duplicate number 
+function findDuplicateNum(arr) {
+  let duplicateNum = [];
+  for (let i = 0; i < arr.length; i++) {
+    // for (let j = i+1; j < arr.length; j++) {   //FOR THIS CODE WE DON'T NEED SLICE TO HALF RESUTL ARR
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) null;   //do not check same is same or not
+      else if (arr[i] === arr[j]) duplicateNum.push(arr[i]);
+
+    }
+
   }
-  console.log(numArr);
-  console.log(charArr);
+  //duplicateNum = [2, 5, 2, 5]
+  let result = duplicateNum.slice(0, duplicateNum.length / 2)
+  console.log(duplicateNum);
+  console.log(result);
 }
 
-seperateNumStr(arr);
 
-
-
+findDuplicateNum(arr);
 
 
 
